d67a29eda11ffa5679b282985751fcbc94631614
David Wright <dontbothertheking@gmail.com>
2019-12-14 16:06:21 -0600

    Change vue indent to spaces

--- vue.js
+++ vue.js
@@ -10,7 +10,7 @@ module.exports = {
 		browser: true,
 	},
 	rules: {
-		'vue/html-indent': [2, 'tab'],
+		'vue/html-indent': [2, 2],
 		'vue/max-attributes-per-line': 0,
 		'vue/html-closing-bracket-newline': [
 			1,
